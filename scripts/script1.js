6aaba4606a632556fb45fc91bd3c63de4b050def
suja2004 <sujan.22cs164@sode-edu.in>
2025-04-19 11:19:45 +0530

    3d

--- scripts/script1.js
+++ scripts/script1.js
@@ -67,7 +67,6 @@
 //   slider.prepend(slider.querySelector('img:last-child'));
 // };
 
-
 // 3D tilting card
 // const pre = document.querySelector("pre");
 
@@ -84,6 +83,26 @@
 
 //   const offsetX = ((x - middleX) / middleX) * 45;
 //   const offsetY = ((y - middleY) / middleY) * -45;
+//   element.style.setProperty("--rotateX", offsetY + "deg");
+//   element.style.setProperty("--rotateY", offsetX + "deg");
+// }
+
+// cube
+// const cube = document.querySelector(".container");
+
+// document.addEventListener("mousemove", (e) => {
+//   rotateEle(e, cube);
+// });
+
+// function rotateEle(event, element) {
+//   const x = event.clientX;
+//   const y = event.clientY;
+
+//   const middleX = window.innerWidth / 2;
+//   const middleY = window.innerHeight / 2;
+
+//   const offsetX = ((x - middleX) / middleX) * 180;
+//   const offsetY = ((y - middleY) / middleY) * -180;
 //   element.style.setProperty("--rotateX", offsetY + "deg");
 //   element.style.setProperty("--rotateY", offsetX + "deg");
 // }
